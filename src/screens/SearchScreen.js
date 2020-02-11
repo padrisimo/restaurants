bf596858135aaf6685f5eeabbf25c29c4c542c55
padrisimo <chamizho@gmail.com>
2020-02-11 17:04:48 +0100

    basic error handlign

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,26 +7,28 @@ import yelp from "../api/yelp";
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
 
   const searchApi = async () => {
-    const response = await yelp.get("/search", {
-      params: {
-        limit: 50,
-        term,
-        location: 'barcelona'
-      }
-    });
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term,
+          location: "barcelona"
+        }
+      });
 
-    setResults(response.data.businesses);
+      setResults(response.data.businesses);
+    } catch (error) {
+      setErrorMessage("Something went wrong");
+    }
   };
 
   return (
     <View>
-      <SearchBar
-        term={term}
-        onTermChange={setTerm}
-        onTermSubmit={searchApi}
-      />
+      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} resuls</Text>
     </View>
   );
