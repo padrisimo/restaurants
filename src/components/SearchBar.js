aefe1ef4a0fd3575b9a66b278c5b7155e88e1416
padrisimo <chamizho@gmail.com>
2020-02-11 12:16:34 +0100

    managin state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,18 @@ import React from "react";
 import { View, TextInput, StyleSheet } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
   return (
     <View style={styles.container}>
-      <AntDesign name="search1"style={styles.icon}/>
-      <TextInput placeholder="search" style={styles.textInput} />
+      <AntDesign name="search1" style={styles.icon} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="search"
+        style={styles.textInput}
+        onChangeText={onTermChange}
+        value={term}
+      />
     </View>
   );
 };
@@ -19,12 +26,12 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     flexDirection: "row",
     paddingHorizontal: 15
-  }, 
+  },
   icon: {
     fontSize: 30,
     alignSelf: "center",
     marginRight: 15
-  }, 
+  },
   textInput: {
     flex: 1,
     fontSize: 18 // default is 14
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text } from "react-native";
 
 import SearchBar from "../components/SearchBar";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState("");
   return (
     <View>
-      <SearchBar />
-      <Text>yea</Text>
+      <SearchBar term={term} onTermChange={newTerm => setTerm(newTerm)} />
+  <Text>yea pedo {term} </Text>
     </View>
   );
 };
