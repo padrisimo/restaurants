9a6be04d369014f782053269b1c84ef3cdd71ff3
padrisimo <chamizho@gmail.com>
2020-02-11 18:04:58 +0100

    grouping results

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -3,10 +3,14 @@ import { View, Text } from "react-native";
 
 import useResults from "../hooks/useResults";
 import SearchBar from "../components/SearchBar";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchApi, results, errorMessage] = useResults();
+
+  const filterResultsByPrice = price =>
+    results.filter(result => result.price === price);
 
   return (
     <View>
@@ -17,6 +21,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} resuls</Text>
+      <ResultsList results={filterResultsByPrice("€")} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice("€€")} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice("€€€")} title="Big Spender" />
     </View>
   );
 };
