851a296d02b7a1b46684f617a6a8ad5d4efc1a91
padrisimo <chamizho@gmail.com>
2020-02-11 21:55:00 +0100

    FlatList rendering

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ResultsList = ({ title, results }) => {
   return (
     <View>
-      <Text style={styles.title}>{title} - {results.length}</Text>
+      <Text style={styles.title}>
+        {title} - {results.length}
+      </Text>
+      <FlatList
+        horizontal={true}
+        data={results}
+        keyExtractor={result => result.id}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+      />
     </View>
   );
 };
@@ -12,7 +20,7 @@ const ResultsList = ({ title, results }) => {
 const styles = StyleSheet.create({
   title: {
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: "bold"
   }
 });
 
